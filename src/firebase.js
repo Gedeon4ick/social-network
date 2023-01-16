caa921936f42337350712bf1401b1c8c55f488d1
Gedeon4ick <pavell720@gmaul.com>
2023-01-16 20:32:47 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNHOwcxwfrGUJWw8ompyqsN3F7mTE1ytw",
+    authDomain: "whats-app-clone-67280.firebaseapp.com",
+    projectId: "whats-app-clone-67280",
+    storageBucket: "whats-app-clone-67280.appspot.com",
+    messagingSenderId: "776940211664",
+    appId: "1:776940211664:web:0d1facdc5193b4cb45bdd5",
+    measurementId: "G-4WFB99QCX6"
+  };
